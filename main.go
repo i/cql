563c5c384c5e45f0c51ec63c9b7c1c344211364e
Ian Lozinski <iml@uber.com>
2016-10-16 17:04:24 -0700

    wip

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main() {
 	conn, err := net.Dial("tcp", "127.0.0.1:9042")
 	fatalIfError("error connecting", err)
 
+	go frameReader(conn)
+
 	frame, err := startupFrame()
 	fatalIfError("error creating startup frame", err)
 	fb, err := frame.bytes()
@@ -18,11 +20,38 @@ func main() {
 	conn.Write(fb)
 	fatalIfError("error writing startup frame", err)
 
-	f, err := readFrame(conn)
-	fatalIfError("error reading", err)
+	select {}
+}
 
-	fmt.Println(f.header)
+func frameReader(c net.Conn) {
+	for {
+		f, err := readFrame(c)
+		if err != nil {
+			log.Printf("error reading frame: %v", err)
+			continue
+		}
 
+		switch f.header.Opcode {
+		case _error:
+		case _startup:
+		case _ready:
+		case _authenticate:
+		case _options:
+		case _supported:
+		case _query:
+		case _result:
+		case _prepare:
+		case _execute:
+		case _register:
+		case _event:
+		case _batch:
+		case _authChallenge:
+		case _authResponse:
+		case _authSuccess:
+		default:
+			log.Printf("got frame with unknown opcode: %x", f.header.Opcode)
+		}
+	}
 }
 
 func fatalIfError(fmtStr string, err error) {
